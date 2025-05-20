dc0295c6416ae0479be207a7cc00d9a86a932406
Igor José de Oliveira <igorjoliveira@outlook.com>
2025-05-19 23:42:33 -0300

    separação da ioc

---
+++ src/backend/Softtek.IoC/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Softtek.Data.Context;
+
+namespace Softtek.IoC
+{
+    namespace Softtek.IoC.DependencyInjection
+    {
+        public static class DependencyInjectionConfig
+        {
+            public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+            {
+                // DbContext
+                services.AddDbContext<SofttekDbContext>(options =>
+                    options.UseSqlite(configuration.GetConnectionString("SofttekDb")));
+
+                return services;
+            }
+        }
+    }
+}
--- src/backend/Softtek.Webapi/Program.cs
+++ src/backend/Softtek.Webapi/Program.cs
@@ -1,30 +1,32 @@
-using Microsoft.EntityFrameworkCore;
-using Softtek.Data.Context;
+using Softtek.IoC.Softtek.IoC.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<SofttekDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("SofttekDb")));
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Softtek.Webapi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            // Add services to the container.
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddControllers();
+            
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
